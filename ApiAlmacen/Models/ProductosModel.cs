10ffe4219a58ed57fe11e15bc9a9a308341172c2
damian suffo <suffodamian02@gmail.com>
2023-06-28 02:13:57 -0300

    modelo de productos agregados

---
+++ ApiAlmacen/Models/ProductosModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiAlmacen.Models
+{
+    public class ProductosModel
+    {
+        public int IDProducto { get; set; }
+        [Required(ErrorMessage = "El campo 'NombreProducto' es requerido")]
+        public string NombreProducto { get; set; }
+        [Required(ErrorMessage = "El campo 'DescripcionProducto' es requerido")]
+        public string DescripcionProducto { get; set; }
+        [Required(ErrorMessage = "El campo 'PesoProducto' es requerido")]
+        public int PesoProducto { get; set; }
+        [Required(ErrorMessage = "El campo 'CantidadProducto' es requerido")]
+        public int CantidadProducto { get; set; }
+    }
+}
