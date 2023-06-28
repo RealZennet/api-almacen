2b2c0440bc483f6ba2e5d0745c60a56e84f69acc
damian suffo <suffodamian02@gmail.com>
2023-06-28 03:04:09 -0300

    modelo de lotes agregado

---
+++ ApiAlmacen/Models/LotesModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiAlmacen.Models
+{
+    public class LotesModels
+    {
+        public int IDLote { get; set; }
+        public int CantidadProductos { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int IDCamionAsignado { get; set; }
+    }
+}
