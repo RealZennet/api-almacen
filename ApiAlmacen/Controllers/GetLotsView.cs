70f89654c2b2d28246d7edffadec792ebb07182e
damian suffo <suffodamian02@gmail.com>
2023-07-12 17:08:07 -0300

    Clase para filtrar datos de los lotes

---
+++ ApiAlmacen/Controllers/GetLotsView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiAlmacen.Controllers
+{
+    public class GetLotsView
+    {
+        public int IDLote { get; set; }
+        public int CantidadProductosEnLote { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        //public int IDCamionAsignado { get; set; }
+    }
+}
