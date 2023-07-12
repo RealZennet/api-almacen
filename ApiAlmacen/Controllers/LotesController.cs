1c7edf183af4fc5a3d96915333a24abf771f846d
damian suffo <suffodamian02@gmail.com>
2023-07-12 17:09:17 -0300

    codigo refactorizado del controlador de lotes para agregar a db

--- ApiAlmacen/Controllers/LotesController.cs
+++ ApiAlmacen/Controllers/LotesController.cs
@@ -9,10 +9,6 @@ namespace ApiAlmacen.Controllers
 {
     public class LotesController:ApiController
     {
-        public static List<LotesModels> DatosLotes = new List<LotesModels>();
-        LotesModels lotes = new LotesModels();
-
-
         [Route("api/lotes")]
         public IHttpActionResult Post([FromBody] LotesModels lote)
         {
@@ -21,43 +17,75 @@ namespace ApiAlmacen.Controllers
                 return BadRequest("Error en el ingreso de datos");
             }
             lote.FechaCreacion = DateTime.Now;
-            DatosLotes.Add(lote);
-            return Ok(lote);
+            lote.Save();
+            return Ok($"Lote {lote.IDLote} fue guardado con exito");
         }
 
         [Route("api/lotes")]
-        public IHttpActionResult Get([FromBody] LotesModels lote)
+        public IHttpActionResult Get()
         {
-            var ListaLotes = DatosLotes.ToList();
-            return Ok(ListaLotes);
+            LotesModels lotes = new LotesModels();
+            var listaLotes = lotes.GetAllLots();
+            var lotsView = listaLotes.Select(everyLot => new GetLotsView
+            {
+                IDLote = everyLot.IDLote,
+                FechaCreacion = everyLot.FechaCreacion,
+                CantidadProductosEnLote = everyLot.CantidadProductosEnLote
+            });
+            return Ok(lotsView);
         }
+
 
         [Route("api/lotes/{id:int}")]
         public IHttpActionResult Get(int id)
         {
-            LotesModels LoteABuscar = DatosLotes.Find(x => x.IDLote == id); 
-            if (LoteABuscar == null)
+            LotesModels lotes = new LotesModels();
+            var listaLotes = lotes.GetAllLots();
+            var lote = listaLotes.FirstOrDefault(everyLot => everyLot.IDLote == id);
+            if (lote == null)
             {
                 return NotFound();
             }
             else
             {
-                return Ok(LoteABuscar);
+                var lotsView = new GetLotsView
+                {
+                    IDLote = lote.IDLote,
+                    FechaCreacion = lote.FechaCreacion,
+                    CantidadProductosEnLote = lote.CantidadProductosEnLote
+                };
+                return Ok(lotsView);
             }
         }
         [Route("api/lotes/{id:int}")]
         public IHttpActionResult Delete(int id)
         {
-            LotesModels LoteABuscar = DatosLotes.Find(x => x.IDLote == id);
-            if (LoteABuscar == null)
+            LotesModels lotes = new LotesModels();
+            var listaLotes = lotes.GetAllLots();
+            var lote = listaLotes.FirstOrDefault(everyLot => everyLot.IDLote == id);
+            if (lote == null)
             {
                 return NotFound();
             }
             else
             {
-                DatosLotes.Remove(LoteABuscar);
-                return Ok($" El lote {LoteABuscar.IDLote} ha sido eliminado");
+                lote.DeleteLots();
+                return Ok($"El lote con el id {lote.IDLote} fue eliminado con exito");
             }
+        }
+
+        [Route("api/lotes/{id:int}")]
+        public IHttpActionResult Put(int id, [FromBody] LotesModels lote)
+        {
+            if (!ModelState.IsValid || lote == null)
+            {
+                return BadRequest("Error en el ingreso de datos");
+            }
+
+            lote.IDLote = id;
+            lote.Edit();
+
+            return Ok($"Producto {lote.IDLote} editado con éxito");
         }
 
     }
