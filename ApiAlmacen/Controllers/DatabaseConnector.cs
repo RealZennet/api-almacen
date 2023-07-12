605a0dcb79fb86d71c76ce39cbbc8ff6786b5f23
damian suffo <suffodamian02@gmail.com>
2023-07-12 16:54:34 -0300

    database connector

---
+++ ApiAlmacen/Controllers/DatabaseConnector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySqlConnector;
+
+namespace ApiAlmacen.Controllers
+{
+    public abstract class DatabaseConnector
+    {
+        public string dbip;
+        public string dbUser;
+        public string dbPassword;
+        public string dbDatabaseName;
+
+        public MySqlConnection Connection;
+        public MySqlCommand Command;
+        public MySqlDataReader Reader;
+
+        public DatabaseConnector()
+        {
+            this.dbip = "localhost";
+            this.dbUser = "root";
+            this.dbPassword = "zackquack";
+            this.dbDatabaseName = "quickCarryApiTesting";
+
+            this.Connection = new MySqlConnection(
+                $"server={this.dbip};" +
+                $"user={this.dbUser};" +
+                $"password={this.dbPassword};" +
+                $"database={this.dbDatabaseName};");
+
+            this.Connection.Open();
+            this.Command = new MySqlCommand();
+            this.Command.Connection = this.Connection;
+        }
+
+    }
+}
--- ApiAlmacen/Models/ProductosModel.cs
+++ ApiAlmacen/Models/ProductosModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiAlmacen.Controllers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,16 +7,13 @@ using System.Web;
 
 namespace ApiAlmacen.Models
 {
-    public class ProductosModel
+    public class ProductosModel : DatabaseConnector
     {
         public int IDProducto { get; set; }
-        [Required(ErrorMessage = "El campo 'NombreProducto' es requerido")]
         public string NombreProducto { get; set; }
-        [Required(ErrorMessage = "El campo 'DescripcionProducto' es requerido")]
         public string DescripcionProducto { get; set; }
-        [Required(ErrorMessage = "El campo 'PesoProducto' es requerido")]
         public int PesoProducto { get; set; }
-        [Required(ErrorMessage = "El campo 'CantidadProducto' es requerido")]
         public int CantidadProducto { get; set; }
+
     }
 }
