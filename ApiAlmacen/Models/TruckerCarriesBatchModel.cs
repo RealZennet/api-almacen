651e6c3e3d663552c87e90d482dbf74c88288026
damian suffo <suffodamian02@gmail.com>
2023-07-30 20:03:02 -0300

    vincular y desvincular lotes a camiones

---
+++ ApiAlmacen/Models/TruckerCarriesBatchModel.cs
@@ -0,0 +1,28 @@
+﻿using ApiAlmacen.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiAlmacen.Models
+{
+    public class TruckerCarriesBatchModel  : DatabaseConnector
+    {
+        public int IDTruck { get; set; }
+        public int IDBatch { get; set; }
+
+        public void Save()
+        {
+            this.Command.CommandText = $"INSERT INTO llevan(id_Cam, id_Lote) = " +
+                $"{this.IDTruck}, " +
+                $"{this.IDBatch} ";
+            this.Command.ExecuteNonQuery();
+        }
+
+        public void DeleteCarrie()
+        {
+            this.Command.CommandText = $"DELETE FROM llevan WHERE id_Cam = {this.IDTruck}";
+        }
+
+    }
+}
