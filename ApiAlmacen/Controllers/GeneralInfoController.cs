72f452ce558ecd0d5742aa3add51417066a26c74
damian suffo <suffodamian02@gmail.com>
2023-11-02 02:06:10 -0300

    endpoint con estadisticas generales del almacen

---
+++ ApiAlmacen/Controllers/GeneralInfoController.cs
@@ -0,0 +1,40 @@
+﻿using ApiAlmacen.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ApiAlmacen.Controllers
+{
+    public class LoteCountController : ApiController
+    {
+        [HttpGet]
+        [Route("api/v1/almacen/generalinfo/products/count")]
+        public IHttpActionResult GetProductCount()
+        {
+            ProductModel ProductModel = new ProductModel();
+            int productCount = ProductModel.GetTotalProductOnStoreHouse();
+
+            Dictionary<string, int> result = new Dictionary<string, int>
+            {
+                { "Count", productCount }
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("api/v1/almacen/generalinfo/batch/count")]
+        public IHttpActionResult GetBatchCount()
+        {
+            BatchModels BatchModel = new BatchModels();
+            int loteCount = BatchModel.GetTotalBatchOnStoreHouse();
+
+            Dictionary<string, int> result = new Dictionary<string, int>
+            {
+                { "Count", loteCount }
+            };
+
+            return Ok(result);
+        }
+    }
+
+}
