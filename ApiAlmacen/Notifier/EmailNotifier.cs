69b22df0d4bf7459663f41da0ad8160261795c02
damian suffo <suffodamian02@gmail.com>
2023-11-05 21:20:24 -0300

    feature para enviar emails terminada

--- ApiAlmacen/Notifier/EmailNotifier.cs
+++ ApiAlmacen/Notifier/EmailNotifier.cs
@@ -10,29 +10,32 @@ namespace ApiAlmacen.Notifier
     {
         protected string email = "quickcarry.correo@gmail.com";
         protected string passwordemail = "hwsnwtkluboprshu";
+        public string emailDestination { get; set; }
+        public string msgcontent { get; set; }
 
         private string subject = "Notificacion desde ZTracking";
 
-        public void SendEmailNotification(string email, string msgcontent)
+        public void SendEmailNotification(string emailDestination, string msgcontent)
         {
             try
             {
                 MailMessage mm = new MailMessage();
                 SmtpClient sc = new SmtpClient("smtp.gmail.com");
-                mm.From = new MailAddress(email);
-                mm.To.Add(email);
+                mm.From = new MailAddress(this.email);
+                mm.To.Add(emailDestination);  
                 mm.Subject = subject;
                 mm.Body = msgcontent;
                 sc.Port = 587;
                 sc.EnableSsl = true;
-                sc.Credentials = new System.Net.NetworkCredential(email, passwordemail);
-                sc.EnableSsl = true;
+                sc.Credentials = new System.Net.NetworkCredential(this.email, this.passwordemail);
                 sc.Send(mm);
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
-                throw new Exception("Error");
+                throw new Exception("Error: " + ex.Message);
             }
         }
+
 
     }
 
