56609616675dbe14f8cfa21f27fe3e5dc83301f1
damian suffo <suffodamian02@gmail.com>
2023-11-05 21:00:02 -0300

    emailnotifier

---
+++ ApiAlmacen/Notifier/EmailNotifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace ApiAlmacen.Notifier
+{
+    public class EmailNotifier
+    {
+        protected string email = "quickcarry.correo@gmail.com";
+        protected string passwordemail = "hwsnwtkluboprshu";
+
+        private string subject = "Notificacion desde ZTracking";
+
+        public void SendEmailNotification(string email, string msgcontent)
+        {
+            try
+            {
+                MailMessage mm = new MailMessage();
+                SmtpClient sc = new SmtpClient("smtp.gmail.com");
+                mm.From = new MailAddress(email);
+                mm.To.Add(email);
+                mm.Subject = subject;
+                mm.Body = msgcontent;
+                sc.Port = 587;
+                sc.EnableSsl = true;
+                sc.Credentials = new System.Net.NetworkCredential(email, passwordemail);
+                sc.EnableSsl = true;
+                sc.Send(mm);
+            }catch (Exception ex)
+            {
+                throw new Exception("Error");
+            }
+        }
+
+    }
+
+}
